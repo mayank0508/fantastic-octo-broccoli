637b77c7f55a50f8258c72a4623acb90a7e27eeb
mayank0508 <thakurmayank0508@gmail.com>
2021-02-17 19:54:57 +0530

    attack value declared

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,13 @@
-const attackValue = 10;
+const ATTACK_VALUE = 10;
 let chooseMaxLife = 100;
+let currentMonsterHealth = chooseMaxLife;
+let currentPlayerHealth = chooseMaxLife;
+
+adjustHealthBars(chooseMaxLife);
+
+function attackHandler() {
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+}
+ 
+attackBtn.addEventListener('click')
