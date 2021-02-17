d14091750f331f3d234fe47024f14a4f1e2e340d
mayank0508 <thakurmayank0508@gmail.com>
2021-02-17 23:43:25 +0530

    strong attack value

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
-const MONSTER_ATTACK_VALUE =14;
+const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 20;
 
 let chooseMaxLife = 100;
 let currentMonsterHealth = chooseMaxLife;
@@ -12,11 +13,29 @@ function attackHandler() {
     currentMonsterHealth -= damage;
     const playerdamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerdamage;
-    if(currentMonsterHealth <= 0 ){
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You Won !');
-    } else if(currentPlayerHealth <= 0){
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
         alert('You Lost !');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('Match Drawn');
     }
 }
- 
+
+function strongAttackHandler() {
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    const playerdamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerdamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You Won !');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You Lost !');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('Match Drawn');
+    }
+}   
+
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
